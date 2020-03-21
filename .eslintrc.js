2c3c6a3771121d46d05720001ad439aa07e4f73a
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro-2.local>
2020-03-20 20:21:01 -0600

    Pre-release commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,33 @@ module.exports = {
     },
     // add your custom rules here
     rules: {
+        'vue/no-v-html': 0,
+        'vue/require-default-prop': 0,
+        'vue/max-attributes-per-line': ['error', {
+            'singleline': 6,
+            'multiline': {
+                'max': 1,
+                'allowFirstLine': false
+            }
+        }],
+        'vue/html-indent': ['error', 4, {
+            'ignores': [],
+            'attribute': 1,
+            'baseIndent': 1,
+            'closeBracket': 0,
+            'alignAttributesVertically': true,
+        }],
+        'vue/html-closing-bracket-newline': ['error', {
+            'singleline': 'never',
+            'multiline': 'never'
+        }],
         // Ensure no space after keywords like if.
-        'keyword-spacing': ["error", {
-            "overrides": {
-                "if": { "after": false },
-                "for": { "after": false },
-                "while": { "after": false }
+        'keyword-spacing': ['error', {
+            'overrides': {
+                'if': { 'after': false },
+                'for': { 'after': false },
+                'while': { 'after': false },
+                'catch': { 'after': false }
             }
         }],
         // Use alternative brace style
@@ -20,7 +41,9 @@ module.exports = {
         // No spaces before parens
         'space-before-function-paren': ['error', 'never'],
         // Indent for tabs because spaces suck
-        'indent': ['error', 4],
+        'indent': ['error', 4, {
+            'ignoredNodes': ['TemplateLiteral']
+        }],
         // Semi colons always where they need to be
         'semi': [2, 'always'],
         // allow paren-less arrow functions
@@ -28,6 +51,6 @@ module.exports = {
         // allow async-await
         'generator-star-spacing': 0,
         // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     }
-}
+};
